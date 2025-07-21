9d13b2d73f414237d692c383779985283276a953
Joshua Almodiel <joshuaalmodiel1133333@gmail.com>
2025-07-21 15:02:11 +0800

    Done

--- almodiel/app/Login.tsx
+++ almodiel/app/Login.tsx
@@ -1,12 +1,43 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-
+import React, { useState } from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity
+} from 'react-native';
 export default function Login() {
+
+  const [emailphone, setEmailPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    if (emailphone && password) {
+      setSubmitted(true);
+    } else {
+      alert('Please fill out all fields.');
+    }
+  };
+
+  const handleClear = () => {
+    setEmail('');
+    setPhone('');
+    setSubmitted(false);
+  };
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 20 }}>
       <Text style={{ fontSize: 28, fontWeight: "bold", position: "absolute", left: 30, top: 20 }}>Sign In</Text>
 
       <TextInput
         placeholder="E-mail or phone number"
+        value={emailphone}
+        onChangeText={(text: string) => setEmailPhone(text)}
         style={{
           width: "100%",
           borderWidth: 1,
@@ -19,7 +50,9 @@ export default function Login() {
 
       <TextInput
         placeholder="Password"
+        value={password}
         secureTextEntry
+        onChangeText={(text: string) => setPassword(text)}
         style={{
           width: "100%",
           borderWidth: 1,
@@ -31,7 +64,7 @@ export default function Login() {
       />
 
       <TouchableOpacity
-        onPress={() => {}}
+        onPress={() => { }}
         style={{
           marginTop: 20,
           backgroundColor: "#1B1F26",
@@ -40,13 +73,15 @@ export default function Login() {
           borderRadius: 8,
         }}
       >
+        <Pressable onPress={handleSubmit}>
         <Text style={{ color: "white", fontWeight: "bold" }}>Log in</Text>
+        </Pressable>
       </TouchableOpacity>
-        
-        <Text style={{ color: "black", fontWeight: "bold", marginTop: 30, marginBottom: 30 }}>OR</Text>
+
+      <Text style={{ color: "black", fontWeight: "bold", marginTop: 30, marginBottom: 30 }}>OR</Text>
 
       <TouchableOpacity
-        onPress={() => {}}
+        onPress={() => { }}
         style={{
           marginTop: 15,
           marginBottom: 75,
@@ -58,6 +93,24 @@ export default function Login() {
       >
         <Text style={{ color: "white", fontWeight: "bold" }}>Log in with Facebook</Text>
       </TouchableOpacity>
+
+      {submitted && (
+              <View style={{
+                display: 'flex',
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                marginTop: 10,
+                marginLeft: -200,
+                gap: 10,
+              }}>
+                <hr />
+                <Text>Submitted Information:</Text>
+                <Text>Email or Phone: {emailphone}</Text>
+                <Text>Password: {password}</Text>
+              </View>
+            )}
     </View>
+
+    
   );
 }
--- almodiel/app/SignUp.tsx
+++ almodiel/app/SignUp.tsx
@@ -1,74 +1,164 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
 
 export default function SignUp() {
+
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    if (name && email && phone && password) {
+      setSubmitted(true);
+    } else {
+      alert('Please fill out all fields.');
+    }
+  };
+
+  const handleClear = () => {
+    setName('');
+    setEmail('');
+    setPhone('');
+    setPassword('');
+    setSubmitted(false);
+  };
+
+
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 20 }}>
-          <Text style={{ fontSize: 28, fontWeight: "bold", position: "absolute", left: 30, top: 20 }}>Create new account</Text>
-    
-          <TextInput
-            placeholder="Full Name"
-            style={{
-              width: "100%",
-              borderWidth: 1,
-              borderColor: "#ccc",
-              borderRadius: 20,
-              padding: 14,
-              marginBottom: 30,
-            }}
-          />
-    
-          <TextInput
-            placeholder="Phone Number"
-            secureTextEntry
-            style={{
-              width: "100%",
-              borderWidth: 1,
-              borderColor: "#ccc",
-              borderRadius: 20,
-              padding: 14,
-              marginBottom: 30,
-            }}
-          />
+    <SafeAreaView>
+      <ScrollView>
+        <Text style={{
+          padding: 14,
+          marginBottom: 30,
+          fontSize: 20,
+          textAlign: 'center',
+          fontWeight: 700
+        }}>User Registration Form</Text>
 
-          <TextInput
-            placeholder="E-mail Address"
-            secureTextEntry
-            style={{
-              width: "100%",
-              borderWidth: 1,
-              borderColor: "#ccc",
-              borderRadius: 20,
-              padding: 14,
-              marginBottom: 30,
-            }}
-          />
+        <TextInput
+          placeholder="Full Name"
+          value={name}
+          onChangeText={(text: string) => setName(text)}
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 20,
+            padding: 14,
+            marginBottom: 30,
+          }}
+        />
 
-          <TextInput
-            placeholder="Password"
-            secureTextEntry
-            style={{
-              width: "100%",
-              borderWidth: 1,
-              borderColor: "#ccc",
-              borderRadius: 20,
-              padding: 14,
-              marginBottom: 30,
-            }}
-          />
-    
+        <TextInput
+          placeholder="Phone Number"
+          value={phone}
+          onChangeText={(text: string) => setPhone(text)}
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 20,
+            padding: 14,
+            marginBottom: 30,
+          }}
+        />
+
+        <TextInput
+          placeholder="E-mail Address"
+          value={email}
+          onChangeText={(text: string) => setEmail(text)}
+          keyboardType="email-address"
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 20,
+            padding: 14,
+            marginBottom: 30,
+          }}
+        />
+
+        <TextInput
+          placeholder="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={(text: string) => setPassword(text)}
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 20,
+            padding: 14,
+            marginBottom: 30,
+          }}
+        />
+
+        <View style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 10,
+        }}>
           <TouchableOpacity
-            onPress={() => {}}
             style={{
-              marginTop: 20,
-              marginBottom: 70,
               backgroundColor: "#1B1F26",
-              paddingVertical: 15,
-              paddingHorizontal: 80,
+              paddingVertical: 10,
+              paddingHorizontal: 40,
+              borderRadius: 8,
+              marginBottom: 12
+            }}
+          >
+            <Pressable onPress={handleSubmit}>
+              <Text style={{ color: "white", fontSize: 16 }}>Submit</Text>
+            </Pressable>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#899BC1",
+              paddingVertical: 10,
+              paddingHorizontal: 40,
               borderRadius: 8,
             }}
           >
-            <Text style={{ color: "white", fontWeight: "bold" }}>Sign Up</Text>
+            <Pressable onPress={handleClear}>
+              <Text style={{ color: "white", fontSize: 16 }}>CLear</Text>
+            </Pressable>
           </TouchableOpacity>
         </View>
+
+        {submitted && (
+          <View style={{
+            display: 'flex',
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            marginTop: 10,
+            gap: 10,
+          }}>
+            <hr />
+            <Text>Submitted Information:</Text>
+            <Text>Full Name: {name}</Text>
+            <Text>Phone Number: {phone}</Text>
+            <Text>Email: {email}</Text>
+            <Text>Password: {password}</Text>
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
